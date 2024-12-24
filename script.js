44c6ac9a765e967fe11061b93b8168c209b7767a
Mayank Sharma <as.mayanksharma@gmail.com>
2024-12-24 19:03:00 +0530

    return the select function

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var board = [
 
 // make a 3x3 board with null as default then make x and o as the actions 
 // player1 and player2 as the objects maybe
+// maybe put board in a IIFE? 
 function Player(name,option,board){
     function isValid(x,y) {
         return board[x][y]==null;
@@ -16,7 +17,12 @@ function Player(name,option,board){
     function select(x,y){
         if (isValid(x,y)){
             board[x][y] = option;
+            console.log(`player ${name} chose to place ${option} to ${x},${y}`);
         }
         else {console.log(`The coordinate is already selected ${board[x][y]}`);}
+    }
+    return {
+        select,
+        
     }
 }
