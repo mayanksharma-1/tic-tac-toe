2ca19dde2e418bc7d13ada5c2679598f236874dd
Mayank Sharma <as.mayanksharma@gmail.com>
2025-01-19 18:27:22 +0530

    format/lint the code

--- script.js
+++ script.js
@@ -7,23 +7,17 @@ function gameBoard() {
 
   function Player(option, board) {
     function isValid(x, y) {
-      return (
-        x >= 0 &&
-        x <= 2 &&
-        y >= 0 &&
-        y <= 2 &&
-        board[x][y] == null
-      );
+      return x >= 0 && x <= 2 && y >= 0 && y <= 2 && board[x][y] == null;
     }
 
     function select(x, y) {
       if (isValid(x, y)) {
         board[x][y] = option;
         console.log(`player chose to place ${option} to ${x},${y}`);
-        return true; 
+        return true;
       } else {
         console.log(`Invalid move. Please enter valid coordinates (0-2).`);
-        return false; 
+        return false;
       }
     }
 
@@ -37,7 +31,7 @@ function gameBoard() {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         if (board[i][j] === null) {
-          return false; 
+          return false;
         }
       }
     }
@@ -47,22 +41,34 @@ function gameBoard() {
   function checkWinner(board) {
     // Check rows
     for (let i = 0; i < 3; i++) {
-      if (board[i][0] && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+      if (
+        board[i][0] &&
+        board[i][0] === board[i][1] &&
+        board[i][1] === board[i][2]
+      ) {
         return { keepPlaying: false, winner: board[i][0] };
       }
     }
 
     // Check columns
     for (let i = 0; i < 3; i++) {
-      if (board[0][i] && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+      if (
+        board[0][i] &&
+        board[0][i] === board[1][i] &&
+        board[1][i] === board[2][i]
+      ) {
         return { keepPlaying: false, winner: board[0][i] };
       }
     }
 
     // Check diagonals
     if (
-      (board[0][0] && board[0][0] === board[1][1] && board[1][1] === board[2][2]) ||
-      (board[2][0] && board[2][0] === board[1][1] && board[1][1] === board[0][2])
+      (board[0][0] &&
+        board[0][0] === board[1][1] &&
+        board[1][1] === board[2][2]) ||
+      (board[2][0] &&
+        board[2][0] === board[1][1] &&
+        board[1][1] === board[0][2])
     ) {
       return { keepPlaying: false, winner: board[1][1] };
     }
@@ -94,12 +100,16 @@ function gameBoard() {
 
     let validMove = false;
     while (!validMove) {
-      const xCoordinate = parseInt(prompt(`X coordinate for ${currentPlayer.option} please (0-2)`));
-      const yCoordinate = parseInt(prompt(`Y coordinate for ${currentPlayer.option} please (0-2)`));
+      const xCoordinate = parseInt(
+        prompt(`X coordinate for ${currentPlayer.option} please (0-2)`)
+      );
+      const yCoordinate = parseInt(
+        prompt(`Y coordinate for ${currentPlayer.option} please (0-2)`)
+      );
       validMove = currentPlayer.select(xCoordinate, yCoordinate);
     }
 
-    console.log(board.board); 
+    console.log(board.board);
 
     const win = board.checkWinner(board.board);
     if (!win.keepPlaying) {
